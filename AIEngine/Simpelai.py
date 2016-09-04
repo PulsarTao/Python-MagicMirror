54554b70b69745e6275a0c095484e11d35d9a845
小V <2571440661@qq.com>
2016-09-04 23:00:15 +0800

    SimpAi 人工智能运算库

---
+++ AIEngine/Simpelai.py
@@ -0,0 +1,30 @@
+from simpleai.search import SearchProblem, astar
+ 
+GOAL = 'FU22s22CK'
+ 
+class HelloProblem(SearchProblem):
+    def actions(self, state):
+        if len(state) < len(GOAL):
+            return list(' 312648712648716578137658148129479821612983ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+        else:
+            return []
+ 
+    def result(self, state, action):
+        return state + action
+ 
+    def is_goal(self, state):
+        return state == GOAL
+ 
+    def heuristic(self, state):
+        # how far are we from the goal?
+        wrong = sum([1 if state[i] != GOAL[i] else 0
+                    for i in range(len(state))])
+        missing = len(GOAL) - len(state)
+        return wrong + missing
+ 
+ 
+problem = HelloProblem(initial_state='')
+result = astar(problem)
+ 
+print result.state
+print result.path()
